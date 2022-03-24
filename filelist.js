3951825e5fe6e61bbd4e1a2dc05a32216aa6c4ce
Everett Melanson <everettmelanson@hotmail.com>
2022-03-24 13:53:24 -0400

    Adds selected file to HTML table
    Adds the selected file from file explorer to Div Table in HTML and stores filename in array

--- filelist.js
+++ filelist.js
@@ -1,6 +1,9 @@
+const path = require("path");
 const ipc = require('electron').ipcRenderer
 
 const browseButton = document.getElementById('browseButton')
+
+const fileList = [];
 
 //Sends open file explorer request to main process
 browseButton.addEventListener('click', function (event) {
@@ -8,7 +11,33 @@ browseButton.addEventListener('click', function (event) {
         ipc.send('open-file-dialog')
         ipc.on('reply', (event, result) => {
             resolve(result);
-            console.log(result[0])
+            fileList.push(result[0]);
+            addRow(result[0]);
         })
     });
 })
+
+function addRow(filepath){
+    const div = document.createElement('div');
+    div.className = 'div-table-row'
+
+    
+    var filename = path.basename(filepath)
+
+
+    div.innerHTML = `
+    <div class="div-table-col"><img style="width:14px; padding-left: 4px; padding-right: 8px;" src="SVG/File   List   Checkbox   Unchecked.svg">` + filename + `</div>
+    <div class="div-table-col"></div>
+    <div class="div-table-col">` + filepath + `</div>
+    `;
+
+    document.getElementById('divTable').appendChild(div);
+
+
+}
+
+function removeRow(input) {
+    document.getElementById('content').removeChild(input.parentNode);
+}
+
+
