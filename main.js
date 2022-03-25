28b82ffba164a979cdab6a0bb6958995c230192c
nataliamariya <natalia.bohulevych@gmail.com>
2022-03-25 19:10:48 -0400

    Added missing code from updated master

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-const { app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const ipc = require('electron').ipcMain
+const dialog = require('electron').dialog
+
+//IPC used by renderer process to open system file explorer (for filelist)
+ipc.on('open-file-dialog', function (event) {
+    dialog.showOpenDialog({
+      properties: ['openFile']
+    }).then(result=>{
+        console.log(result.filePaths)
+        var filepath = result.filePaths
+        event.sender.send('browse-reply', filepath);
+    })
+});
+
+//IPC used by renderer process to open system file explorer (for rulefilter files)
+ipc.on('open-rulefilter-txt', function (event) {
+    dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'text', extensions: ['txt'] }]
+    }).then(result=>{
+        console.log(result.filePaths)
+        var filepath = result.filePaths
+        event.sender.send('rulefilter-open-reply', filepath);
+    })
+});
 
 const loadMainWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -9,11 +34,12 @@ const loadMainWindow = () => {
         minHeight: 800,
         center: true,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
         }
     });
     mainWindow.loadFile(path.join(__dirname, "index.html"));
-    
 }
 
 app.on("ready", loadMainWindow);
@@ -26,7 +52,6 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        loadMainWindow();
+        loadMainWindow();  
     }
 });
-
