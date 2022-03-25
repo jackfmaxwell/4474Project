c58f98732de431d69bad78ae2bd6b8f2fbff64ce
nataliamariya <natalia.bohulevych@gmail.com>
2022-03-25 18:50:29 -0400

    Initial commit
    scrolling fixed in table, added empty slots, set window minimum size, centered window

--- main.js
+++ main.js
@@ -1,43 +1,19 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow} = require("electron");
 const path = require("path");
-const ipc = require('electron').ipcMain
-const dialog = require('electron').dialog
-
-//IPC used by renderer process to open system file explorer (for filelist)
-ipc.on('open-file-dialog', function (event) {
-    dialog.showOpenDialog({
-      properties: ['openFile']
-    }).then(result=>{
-        console.log(result.filePaths)
-        var filepath = result.filePaths
-        event.sender.send('browse-reply', filepath);
-    })
-});
-
-//IPC used by renderer process to open system file explorer (for rulefilter files)
-ipc.on('open-rulefilter-txt', function (event) {
-    dialog.showOpenDialog({
-      properties: ['openFile'],
-      filters: [{ name: 'text', extensions: ['txt'] }]
-    }).then(result=>{
-        console.log(result.filePaths)
-        var filepath = result.filePaths
-        event.sender.send('rulefilter-open-reply', filepath);
-    })
-});
 
 const loadMainWindow = () => {
     const mainWindow = new BrowserWindow({
         width : 1200,
         height: 800,
+        minWidth : 1100,
+        minHeight: 800,
+        center: true,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
+            nodeIntegration: true
         }
-
     });
     mainWindow.loadFile(path.join(__dirname, "index.html"));
+    
 }
 
 app.on("ready", loadMainWindow);
@@ -51,7 +27,6 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         loadMainWindow();
-        
     }
 });
 
