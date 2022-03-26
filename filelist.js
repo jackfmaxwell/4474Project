607ec0037cb38af2f863e788199f1d136c4ca284
Everett Melanson <everettmelanson@hotmail.com>
2022-03-26 13:08:55 -0400

    Added check box event listeners

--- filelist.js
+++ filelist.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { workerData } = require("worker_threads");
 const ipc = require('electron').ipcRenderer
 
 const browseButton = document.getElementById('browseButton')
@@ -30,6 +31,11 @@ openButton.addEventListener('click', function (event) {
     });
 })
 
+
+//adds checkbox event listener to starting files in list
+checkboxEventAdder();
+
+//adds files to HTML list and includes event listeners
 function addRow(filepath){
     const div = document.createElement('div');
     div.className = 'div-table-row';
@@ -38,14 +44,35 @@ function addRow(filepath){
     var filename = path.basename(filepath);
 
     div.innerHTML = `
-    <div class="div-table-col"><img style="width:14px; padding-left: 4px; padding-right: 8px;" src="SVG/File   List   Checkbox   Unchecked.svg">` + filename + `</div>
+    <div class="div-table-col-image"><img style="width:14px; padding-left: 4px; padding-right: 8px;" src="SVG/File   List   Checkbox   Unchecked.svg">` + filename + `</div>
     <div class="div-table-col"></div>
     <div class="div-table-col">` + filepath + `</div>
     `;
 
     document.getElementById('divTable').appendChild(div);
+
+    checkboxEventAdder();
+
 }
 
 function removeRow(id) {
     document.getElementById(id).removeChild(input.parentNode);
+}
+
+//adds event listener to check boxes in file list
+function checkboxEventAdder(){
+    const check_boxes = document.querySelectorAll('.div-table-col-image');
+    console.log(check_boxes);
+
+    check_boxes.forEach(function(check_box){
+        check_box.addEventListener('click', function (event) {
+            if (!check_box.firstChild.src.includes('Un')){
+                console.log("should uncheck here")
+                check_box.firstChild.src = "SVG/File   List   Checkbox   Unchecked.svg";
+            } else {
+                console.log(check_box.firstChild.src)
+                check_box.firstChild.src = "SVG/File   List   Checkbox   Checked.svg";
+            }
+        });
+    })
 }
